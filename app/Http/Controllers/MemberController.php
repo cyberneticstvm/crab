3919a5dd8e7dd17fa8e397b7f3626d73bab943f5
Cybernetics <mail@cybernetics.me>
2025-10-10 14:06:56 +0530

    small updates

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -67,8 +67,7 @@ class MemberController extends Controller
     {
         $member = Member::findOrFail(decrypt($id));
         $pcodes = Country::pluck('phone', 'phone');
-        $type = $member->type;
-        return view('member.edit', compact('member', 'pcodes', 'type'));
+        return view('member.edit', compact('member', 'pcodes'));
     }
 
     /**
