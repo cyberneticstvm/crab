d97d553d4c6b80fae0f7761caca4248908a88581
Cybernetics <mail@cybernetics.me>
2025-10-12 16:26:28 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -128,7 +128,7 @@
     <!-- Plugins JS Ends-->
     <!-- Theme js-->
     <script src="{{ asset('/assets/js/script.js') }}"></script>
-    <script src="{{ asset('/assets/js/theme-customizer/customizer.js') }}"></script>
+    <!--<script src="{{ asset('/assets/js/theme-customizer/customizer.js') }}"></script>-->
     <!-- login js-->
     <!-- Plugin used-->
     @include("message")
