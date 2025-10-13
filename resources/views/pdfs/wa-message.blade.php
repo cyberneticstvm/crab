5703e32df1af43dde01fa20d2d76102d72d5a438
Cybernetics <mail@cybernetics.me>
2025-10-13 08:03:21 +0530

    small updates

--- resources/views/pdfs/wa-message.blade.php
+++ resources/views/pdfs/wa-message.blade.php
@@ -141,12 +141,12 @@
                 <pre />
                 <pre />
                 <pre />
-                <div class="text-end txt-small">Date: {{ date('d.M.Y h:i a') }}</div>
+                <div class="text-end txt-small style: font-family: Arial, Helvetica, sans-serif;">Date: {{ date('d.M.Y h:i a') }}</div>
                 <div style="white-space:wrap">
-                    <p class="text-center font-big fw-bold">{!! $message->title !!}</p>
+                    <p class="text-center font-big fw-bold style: font-family: Arial, Helvetica, sans-serif;">{!! $message->title !!}</p>
                 </div>
                 <div style="white-space:wrap">
-                    <p class="">{!! $message->message !!}</p>
+                    <p class="style: font-family: Arial, Helvetica, sans-serif;">{!! $message->message !!}</p>
                 </div>
             </div>
         </div>
