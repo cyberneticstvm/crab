ffdd69a75a7fce6cb1379f7731dd4becabd135f0
Cybernetics <mail@cybernetics.me>
2025-11-02 23:45:33 +0530

    Small Updates

--- database/migrations/2025_11_01_080242_create_custom_messages_table.php
+++ database/migrations/2025_11_01_080242_create_custom_messages_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('custom_messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('message_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('phone_code', 5)->nullable();
             $table->string('mobile', 15);
