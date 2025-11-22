a24697582d19bc4e4f47d3f23fafefe37c3662c3
Cybernetics <mail@cybernetics.me>
2025-11-22 10:28:54 +0530

    first commit

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -40,6 +40,7 @@ class MessageController extends Controller
         try {
             $inputs = $request->all();
             $inputs['type'] = $type;
+            $inputs['is_signed'] = ($request->is_sidned) ?? null;
             $inputs['created_by'] = Auth::user()->id;
             $inputs['updated_by'] = Auth::user()->id;
             Message::create($inputs);
@@ -78,6 +79,7 @@ class MessageController extends Controller
         try {
             $message = Message::findOrFail(decrypt($id));
             $inputs = $request->all();
+            $inputs['is_signed'] = ($request->is_sidned) ?? null;
             $inputs['updated_by'] = Auth::user()->id;
             $message->update($inputs);
         } catch (Exception $e) {
