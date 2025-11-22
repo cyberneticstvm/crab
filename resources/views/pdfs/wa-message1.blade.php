829163ec7b71472da637657f07d5f1056c09cdb6
Cybernetics <mail@cybernetics.me>
2025-11-22 10:23:13 +0530

    first commit

--- resources/views/pdfs/wa-message1.blade.php
+++ resources/views/pdfs/wa-message1.blade.php
@@ -70,7 +70,7 @@
                 </div>
                 @if($message->is_signed)
                 <div class="text-start" style="margin-left: 25%;">
-                    <img src="{{ asset('/assets/docs/crab-sign.png') }}" width="25%" />
+                    <img src="{{ asset('/assets/docs/crab-sign.png') }}" />
                 </div>
                 @endif
             </div>
