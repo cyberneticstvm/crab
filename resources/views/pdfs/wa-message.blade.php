3302b79eb3a45d23d4ec82e184fd6dd6569aa59f
Cybernetics <mail@cybernetics.me>
2025-11-23 09:37:34 +0530

    small updates

--- resources/views/pdfs/wa-message.blade.php
+++ resources/views/pdfs/wa-message.blade.php
@@ -67,7 +67,7 @@
                 </div>
                 @if($message->is_signed)
                 <div class="text-start">
-                    <img src="{{ asset('/assets/docs/crab-sign.png') }}" width="00%" />
+                    <img src="{{ asset('/assets/docs/crab-sign.png') }}" width="30%" />
                 </div>
                 @endif
             </div>
